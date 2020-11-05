50dda7cce7885227fe46e468d6c5086b9c8284d1
Dorian Bögehold <dorian-andrej.boegehold@groz-beckert.com>
2020-11-05 11:03:29 +0100

    upgrade price

--- functions.js
+++ functions.js
@@ -1,14 +1,23 @@
 var cookies=0;
 var AddCookies=1;
-var Upgrade=2
-var Autoclick=1
+var Upgrade=2;
+var Autoclick=1;
+var upprice=30;
 function Add(){
 cookies=cookies+AddCookies
 document.getElementById("total").innerHTML=cookies
 }
 document.getElementById("upgrbtn").onclick = function() {upgrd()};
 function upgrd() {
+    if (cookies>=upprice){
     alert("You just purchased an upgrade")
     AddCookies=AddCookies*Upgrade
+    cookies=cookies-upprice
+    upprice=upprice*5
+    document.getElementById("upgpr").innerHTML = upprice
+    }
+    else{
+    alert("Not enough cookies")
+    }
 }
 
