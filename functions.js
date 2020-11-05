6be97f5d1ec30a91fd855f1f6bc27fdd1ecaeea0
Dorian Bögehold <dorian-andrej.boegehold@groz-beckert.com>
2020-11-05 14:31:48 +0100

    scripting autoclicker

--- functions.js
+++ functions.js
@@ -1,23 +1,55 @@
 var cookies=0;
+
 var AddCookies=1;
+
 var Upgrade=2;
+
 var Autoclick=1;
+
 var upprice=30;
+
+var autoprice=60;
+
 function Add(){
 cookies=cookies+AddCookies
 document.getElementById("total").innerHTML=cookies
 }
+
 document.getElementById("upgrbtn").onclick = function() {upgrd()};
-function upgrd() {
-    if (cookies>=upprice){
-    alert("You just purchased an upgrade")
-    AddCookies=AddCookies*Upgrade
-    cookies=cookies-upprice
-    upprice=upprice*5
-    document.getElementById("upgpr").innerHTML = upprice
+function upgrd() 
+{
+    if (cookies>=upprice)
+    {
+        alert("You just purchased an upgrade")
+        AddCookies=AddCookies*Upgrade
+        cookies=cookies-upprice
+        upprice=upprice*5
+        document.getElementById("upgpr").innerHTML = upprice
     }
-    else{
-    alert("Not enough cookies")
+    else
+    {
+        alert("Not enough cookies")
     }
 }
 
+document.getElementById("acpr").onclick = function() {auto()};
+function auto(){
+    if (cookies>=autoprice)
+    {
+        cookies=cookies-autoprice
+        window.setInterval(
+        function clicker()
+        {
+            cookies=cookies+Autoclick
+            document.getElementById("acpr").innerHTML = autoprice
+        }
+                ,1000);
+        alert("You just purchased an upgrade")
+        autoprice=autoprice*8
+        Autoclick=Autoclick*Upgrade
+    }
+    else
+    {
+        alert("Not enough cookies")
+    }
+}
