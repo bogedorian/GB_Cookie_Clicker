1ed4a37d1502e07af4ac03a857485f243a52a706
Dorian Bögehold <dorian-andrej.boegehold@groz-beckert.com>
2020-11-12 07:30:57 +0100

    ES6 Module

---
+++ module.js
@@ -0,0 +1,12 @@
+import { Add, upgrd, auto, cookies, AddCookies, Upgrade, Autoclick, upprice, autoprice } from "./functions.js"
+
+const cookies = cookies
+const AddCookies = AddCookies
+const Upgrade = Upgrade
+const Autoclick = Autoclick
+const upprice = upprice
+const autoprice = autoprice
+
+Add()
+upgrd()
+auto()
